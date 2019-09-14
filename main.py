74367da0438816daac8268f834c5b1b433d52e69
Mariano <marianoacosta.003@gmail.com>
2019-09-14 20:42:40 -0300

    spectrum v2

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from numpy import genfromtxt
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+import scipy.io
+from scipy.signal import convolve as sig_convolve, fftconvolve, lfilter, firwin
+from scipy.ndimage import convolve1d
 
 #my_data = genfromtxt('F:\Programacion\Python\Deep Learning\Competition\DATASET\\train.csv', delimiter=',')
 
@@ -9,8 +12,13 @@ data = pd.read_csv('F:\Programacion\Python\Deep Learning\Competition\DATASET\\tr
 
 my_data= data.to_numpy()
 
+low = scipy.io.loadmat('lowpass.mat')
+filt = low['Num']
+
+#RUN FROM HERE
+
 #filter by experiment
-state = "DA" # "CA","DA", "SS"
+state = "SS" # "CA","DA", "SS"
 
 exp = my_data[my_data[:,1]==state]
 
@@ -27,16 +35,17 @@ p1 = p1[p1[:,3]==seat]
 
 p1 = p1[p1[:,2].argsort()]
 
-start = 1024*10
-points = 1024
+start = 1024*18
+points = 1024*4
 numOfplot = 1
 freq = np.array([x for x in range(int(points/2)-1)])*256/points
 
 
-i = 5
+i = 17
     
 signal = np.array(p1[start:start+points-1,i+4],dtype=float)
-spec = 20*np.log10(abs(np.multiply(np.fft.fft(signal,points),np.hamming(points)))[0:int(points/2)-1])   
+window = 1#np.hamming(points)
+spec = 20*np.log10(abs(np.multiply(np.fft.fft(signal/1000,points),window))[0:int(points/2)-1])   
 
 plt.figure()
 
@@ -44,10 +53,15 @@ plt.subplot(3,1,1)
 plt.plot(freq,spec)
 plt.ylabel("Magnitude [dB]")
 plt.xlabel("Frequency [Hz]")
+axes=plt.gca()
 
 
 plt.subplot(3,1,2)
 plt.plot(p1[start:start+points-1,2],signal)
+
+#plt.subplot(4,1,3)
+#ss = np.convolve(signal,filt,mode='valid')
+#plt.plot(p1[start:start+points-1,2],ss)
 
 plt.subplot(3,1,3)
 plt.plot(p1[start:start+points-1,2],p1[start:start+points-1,27])
