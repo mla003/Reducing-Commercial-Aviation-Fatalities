e932795d4edac2303282618b83043bc9b40f5d81
Mariano <marianoacosta.003@gmail.com>
2019-09-14 20:04:08 -0300

    analisis espectral

--- main.py
+++ main.py
@@ -10,13 +10,14 @@ data = pd.read_csv('F:\Programacion\Python\Deep Learning\Competition\DATASET\\tr
 my_data= data.to_numpy()
 
 #filter by experiment
-exp1 = my_data[my_data[:,1]=="CA"]
-exp2 = my_data[my_data[:,1]=="DA"]
-exp3 = my_data[my_data[:,1]=="SS"]
+state = "DA" # "CA","DA", "SS"
+
+exp = my_data[my_data[:,1]==state]
+
 
 #filter by pair of pilot
 pair = 1
-p1 = exp1[exp1[:,0]== pair]
+p1 = exp[exp[:,0]== pair]
 
 #filter by seats
 
@@ -26,16 +27,28 @@ p1 = p1[p1[:,3]==seat]
 
 p1 = p1[p1[:,2].argsort()]
 
-
-points = 50000
+start = 1024*10
+points = 1024
 numOfplot = 1
+freq = np.array([x for x in range(int(points/2)-1)])*256/points
 
 
-
-for i in range(numOfplot):
-    plt.subplot(2,1,1)
+i = 5
     
-    plt.plot(p1[0:points-1,2],p1[0:points-1,i+4])
-    plt.subplot(2,1,2)
-    plt.plot(p1[0:points-1,2],p1[0:points-1,27])
+signal = np.array(p1[start:start+points-1,i+4],dtype=float)
+spec = 20*np.log10(abs(np.multiply(np.fft.fft(signal,points),np.hamming(points)))[0:int(points/2)-1])   
+
+plt.figure()
+
+plt.subplot(3,1,1)
+plt.plot(freq,spec)
+plt.ylabel("Magnitude [dB]")
+plt.xlabel("Frequency [Hz]")
+
+
+plt.subplot(3,1,2)
+plt.plot(p1[start:start+points-1,2],signal)
+
+plt.subplot(3,1,3)
+plt.plot(p1[start:start+points-1,2],p1[start:start+points-1,27])
 
